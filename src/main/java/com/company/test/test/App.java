f1aa4ba26c65536a18447cfe3e5d00690c9b18c0
zhoujijie <928818632@qq.com>
2017-09-20 10:24:30 +0800

    777

--- src/main/java/com/company/test/test/App.java
+++ src/main/java/com/company/test/test/App.java
@@ -7,6 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!111222333444" );
+        System.out.println( "Hello World!" );
     }
 }
